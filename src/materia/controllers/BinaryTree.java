d396ee2e26456883c5751238e3136eb4dd5e7410
Edu-Burbi <eduburba2002@gmail.com>
2025-06-17 19:47:09 -1000

    Trabajo finalizado

---
+++ src/materia/controllers/BinaryTree.java
@@ -0,0 +1,65 @@
+package materia.controllers;
+
+import materia.controllers.models.Node;
+
+public class BinaryTree {
+    private Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    private Node insertRec(Node padre, int value) {
+        if (padre == null) {
+            return new Node(value);
+        }
+        if (value < padre.getValue()) {
+            padre.setLeft(insertRec(padre.getLeft(), value));
+        } else if (value > padre.getValue()) {
+            padre.setRight(insertRec(padre.getRight(), value));
+        }
+        return padre;
+    }
+
+    public void imprimirArbol() {
+        System.out.print("Preorden: ");
+        printPreOrder(root);
+        System.out.println();
+
+        System.out.print("Inorden: ");
+        printInOrder(root);
+        System.out.println();
+
+        System.out.print("Postorden: ");
+        printPostOrder(root);
+        System.out.println();
+    }
+
+    private void printPreOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.getValue() + " ");
+            printPreOrder(node.getLeft());
+            printPreOrder(node.getRight());
+        }
+    }
+
+    private void printInOrder(Node node) {
+        if (node != null) {
+            printInOrder(node.getLeft());
+            System.out.print(node.getValue() + " ");
+            printInOrder(node.getRight());
+        }
+    }
+
+    private void printPostOrder(Node node) {
+        if (node != null) {
+            printPostOrder(node.getLeft());
+            printPostOrder(node.getRight());
+            System.out.print(node.getValue() + " ");
+        }
+    }
+}
